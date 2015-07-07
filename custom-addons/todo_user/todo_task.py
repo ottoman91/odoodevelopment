5f55eed8f0f6c01cf31b2753c9d5723d9c008567
Usman Khaliq <usman.khaliq91@gmail.com>
2015-07-07 08:29:00 +0000

    second commit

--- custom-addons/todo_user/todo_task.py
+++ custom-addons/todo_user/todo_task.py
@@ -1,21 +1,15 @@
+
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-class ToDoTask(models.Model):
-	_inherit = 'todo.task'
-	user_id = fields.Many2one('res.users', 'Responsible')
-	date_deadline = fields.Date('Deadline')
-	name = fields.Char(help="what needs to be done?")
-	@api.multi
-	def do_clear_done(self):
-		domain = [('is_done', '=', True),
-		    '|', ('user_id','=', self.env.uid),
-	            ('user_id', '=', False)]
-		done_recs = self.search(domain)
-		done_recs.write({'active': False})
-		return True 
-	@api.one
-	def do_toggle_done(self):
-		if self.user_id != self.env.user:
-			raise Exception('Only the responsible can do this!')
-		else:
-			return super(TodoTask, self).do_toggle_done()
+
+class Tag(models.Model):
+	_name = 'todo.task.tag'
+	name = fields.Char('Name', 40, translate=True)
+
+class Stage(models.Model):
+	_name = 'todo.task.stage'
+	_order = 'sequence,name'
+	_rec_name = 'name'
+	_table = 'todo_task_stage'
+	name = fields.Char('Name',40,translate=True)
+	sequence = fields.Integer('Sequence')
